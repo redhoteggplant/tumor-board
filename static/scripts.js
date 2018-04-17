17b9e905ed73ca46f36915a29e8028ad78946436
Nadia Irwanto <nadia.irwanto@yale.edu>
2018-04-17 16:43:34 -0400

    user can now change circle color

--- static/scripts.js
+++ static/scripts.js
@@ -1,17 +1,33 @@
 $(document).ready(function(){
-
-	function greenCircle() {
-		return $(document.createElement('span')).addClass('green-circle')
-	}
 	function blueCircle() {
 		return $(document.createElement('span')).addClass('blue-circle')
-	}
-	function yellowCircle() {
-		return $(document.createElement('span')).addClass('yellow-circle')
 	}
 
 	$('#liver').annotatableImage(blueCircle);
 
-	var annotation = $('#annotation').find(":selected").text();
 	// $('#liver span.circle').seralizeAnnotations();
 });
+
+function greenCircle() {
+	return $(document.createElement('span')).addClass('green-circle')
+}
+function blueCircle() {
+	return $(document.createElement('span')).addClass('blue-circle')
+}
+function yellowCircle() {
+	return $(document.createElement('span')).addClass('yellow-circle')
+}
+
+function changeCircle() {
+	// var option = $('#annotation').find(":selected").value;
+	var select = document.getElementById("annotation");
+	var option = select.options[e.selectedIndex].value;
+	console.log(option);
+	if (option == 1) {
+		$('#liver').annotatableImage(blueCircle);
+	} else if (option == 2) {
+		$('#liver').annotatableImage(greenCircle);
+	} else {
+		$('#liver').annotatableImage(yellowCircle);
+	}
+}
