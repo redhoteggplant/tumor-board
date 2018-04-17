7c97fffe5dc056a4b522eafcbaa925de8d866a2e
Nadia Irwanto <nadia.irwanto@yale.edu>
2018-04-17 14:13:54 -0400

    added more circle colors

--- static/scripts.js
+++ static/scripts.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-	function blackCircle() {
-		return $(document.createElement('span')).addClass('black-circle')
+
+	function greenCircle() {
+		return $(document.createElement('span')).addClass('green-circle')
 	}
-	$('#liver').annotatableImage(blackCircle);
+	function blueCircle() {
+		return $(document.createElement('span')).addClass('blue-circle')
+	}
+	function yellowCircle() {
+		return $(document.createElement('span')).addClass('yellow-circle')
+	}
+
+	$('#liver').annotatableImage(blueCircle);
+
+	var annotation = $('#annotation').find(":selected").text();
+	// $('#liver span.circle').seralizeAnnotations();
 });
